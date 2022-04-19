a02932d71ba4a5509b7aa683beb842457cf55526
jacobsickels <jacobraysickels@gmail.com>
2022-04-18 21:45:18 -0500

    Add links and styles for dle games

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.nytimes.com/games/wordle/index.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wor<strong>dle</strong>
         </a>
+        <a
+          className="App-link"
+          href="https://worldle.teuteuf.fr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Worl<strong>dle</strong>
+        </a>
+        <a
+          className="App-link"
+          href="https://www.heardle.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hear<strong>dle</strong>
+        </a>
+        <a
+          className="App-link"
+          href="https://nerdlegame.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ner<strong>dle</strong>
+        </a>
+        <a
+          className="App-link"
+          href="https://qntm.org/files/absurdle/absurdle.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Absur<strong>dle</strong>
+        </a>
+        <div>
+          <a
+            className="nsfw"
+            href="https://www.lewdlegame.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lew<strong>dle</strong>
+          </a>
+          &nbsp;
+          <span className="nsfw mention">nsfw</span>
+        </div>
+        <p className="mention">
+          Special mention to&nbsp;
+          <a
+            className="App-link"
+            href="https://semantle.novalis.org//"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Semantle
+          </a>
+        </p>
       </header>
     </div>
   );
